170d52057877e5fdd6de5a2f436c84eaa0239c13
makhameng <makhameng@gmail.com>
2019-05-30 06:34:22 +0200

    added data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) values ('Lesotho');
+INSERT INTO category (description) values ('Maputo');
+INSERT INTO category (description) values ('Limpopo');
+INSERT INTO category (description) values ('Tshisa nyama');
+INSERT INTO unit_of_measure (description) values ('cup');
+INSERT INTO unit_of_measure (description) values ('Teaspoon');
+INSERT INTO unit_of_measure (description) values ('pinch');
+INSERT INTO unit_of_measure (description) values ('ounce');
+INSERT INTO unit_of_measure (description) values ('TableSpoon');
