1c3f277b8503732d1f78f2a7997725fd2e6da6fb
makhameng <makhameng@gmail.com>
2019-05-30 05:56:13 +0200

    added Repositories and category updated Recipe.

---
+++ src/main/java/guru/springframework/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ */
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
