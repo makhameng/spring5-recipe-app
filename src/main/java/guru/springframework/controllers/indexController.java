a173877a6df13f9acfa84fe93674ec3805ca056a
makhameng <makhameng@gmail.com>
2019-06-04 05:20:34 +0200

    Data jpa Query methods.

---
+++ src/main/java/guru/springframework/controllers/indexController.java
@@ -0,0 +1,37 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+/**
+ *
+ */
+@Controller
+public class indexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public indexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Lesotho");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("pinch");
+
+        System.out.println("Cat Id is:" + categoryOptional.get().getId());
+        System.out.println("UOM ID is:" + unitOfMeasureOptional.get().getId());
+
+        return "index";
+    }
+}
